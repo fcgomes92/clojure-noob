5328183924220e5e2975515d6525ffae1db33fd7
Fernando Gomes <fernando.gomes@rtbhouse.com>
2020-01-23 09:19:04 +0100

    added some examples from the book

--- src/clojure_noob/core.clj
+++ src/clojure_noob/core.clj
@@ -4,4 +4,34 @@
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (println "I'm a teapot!"))
+
+(defn multi-arity
+  ;; 2-arity
+  ([target, move]
+   (str "I " move " " target))
+  ([target]
+   (multi-arity target "attack"))
+  ([]
+   (multi-arity "you" "attack")))
+
+(defn calling-everyone-communication
+  [person]
+  (str "Calling " person))
+
+(defn calling-everyone
+  ([& people]
+   (map calling-everyone-communication people)))
+
+(defn calling-everyone-tog
+  [& people]
+  (str "Calling: " (clojure.string/join ", " people)))
+
+(defn chooser
+  ([l]
+   (chooser "anony" l))
+  ([name [fst-choice snd-choice & rest]]
+   (println (str "Hello, " name))
+   (println (str "First choice: " fst-choice))
+   (println (str "Second choice: " snd-choice))
+   (println (str "The rest: " (clojure.string/join ", " rest)))))
