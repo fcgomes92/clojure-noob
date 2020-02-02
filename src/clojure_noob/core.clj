2352ca1037b4b7157bd55fd0b1fa9c080a61b118
Fernando Gomes <fernando.gomes@rtbhouse.com>
2020-02-02 17:47:27 +0100

    #Hobbit Violence

--- src/clojure_noob/core.clj
+++ src/clojure_noob/core.clj
@@ -35,3 +35,78 @@
    (println (str "First choice: " fst-choice))
    (println (str "Second choice: " snd-choice))
    (println (str "The rest: " (clojure.string/join ", " rest)))))
+
+(map (fn [name] (str "Hi, mate " name "!"))
+     ["Mate", "Mati", "Barney"])
+
+((fn [x] (* x 3)) 8)
+
+(defn inc-maker
+  "Create a custom incrementor"
+  [inc-by]
+  #(+ % inc-by))
+
+(def inc3 (inc-maker 3))
+
+(inc3 10)
+
+(def asym-hobbit-body-parts [{:name "head" :size 3}
+                             {:name "left-eye" :size 1}
+                             {:name "left-ear" :size 1}
+                             {:name "mouth" :size 1}
+                             {:name "nose" :size 1}
+                             {:name "neck" :size 2}
+                             {:name "left-shoulder" :size 3}
+                             {:name "left-upper-arm" :size 3}
+                             {:name "chest" :size 10}
+                             {:name "back" :size 10}
+                             {:name "left-forearm" :size 3}
+                             {:name "abdomen" :size 6}
+                             {:name "left-kidney" :size 1}
+                             {:name "left-hand" :size 2}
+                             {:name "left-knee" :size 2}
+                             {:name "left-thigh" :size 4}
+                             {:name "left-lower-leg" :size 3}
+                             {:name "left-achilles" :size 1}
+                             {:name "left-foot" :size 2}])
+
+(defn matching-part
+  [part]
+  {:name (clojure.string/replace (:name part) #"^left-" "right-")
+   :size (:size part)})
+
+(defn symmetrize-body-parts
+  "Expects a seq of maps that have a :name and a :size"
+  [asym-body-parts]
+  (loop [remaining-asym-parts asym-body-parts final-body-parts []]
+    (if (empty? remaining-asym-parts)
+      final-body-parts
+      (let [[part & remaining] remaining-asym-parts]
+        (recur remaining
+               (into final-body-parts
+                     (set [part (matching-part part)])))))))
+
+(defn match-hobbit-parts [acc part]
+  (into acc
+        (set [part (matching-part part)])))
+
+(defn symmetrize-body-parts-reduce
+  [asym-body-parts]
+  (reduce match-hobbit-parts [] asym-body-parts))
+
+(loop [iteration 0]
+  (println (str "Ieration: " iteration))
+  (if (> iteration 3)
+    (println "Goodbye")
+    (recur (inc iteration))))
+
+(defn recursive-printer
+  ([]
+   (recursive-printer 0))
+  ([iteration]
+   (println iteration)
+   (if (> iteration 3)
+     (println "Goodbye")
+     (recursive-printer (inc iteration)))))
+
+(defn)
